75010c584b8f313bfc8607baca8f279a96d68ab2
Khoder <132526568+khoderdev@users.noreply.github.com>
2024-05-10 00:08:58 +0300

    Implemented Lock Screen Logic

--- src/pages/POSPage.jsx
+++ src/pages/POSPage.jsx
@@ -1,33 +1,33 @@
-import React, { useEffect, useRef, useState } from 'react'
-import MainLayout from '../layouts/MainLayout'
-import axios from "axios"
+import React, { useEffect, useRef, useState } from 'react';
+import MainLayout from '../layouts/MainLayout';
+import axios from "axios";
 import { toast } from 'react-toastify';
 import { ComponentToPrint } from '../components/ComponentToPrint';
 import { useReactToPrint } from 'react-to-print';
 
 function POSPage() {
-
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [cart, setCart] = useState([]);
   const [totalAmount, setTotalAmount] = useState(0);
+  const [showPrintPreview, setShowPrintPreview] = useState(false);
 
   const toastOptions = {
     autoClose: 400,
     pauseOnHover: true,
-  }
+  };
 
   const fetchProducts = async () => {
     setIsLoading(true);
     const result = await axios.get('products');
     setProducts(await result.data);
     setIsLoading(false);
-  }
+  };
 
   const addProductToCart = async (product) => {
     // check if the adding product exist
     let findProductInCart = await cart.find(i => {
-      return i.id === product.id
+      return i.id === product.id;
     });
 
     if (findProductInCart) {
@@ -39,8 +39,8 @@ function POSPage() {
           newItem = {
             ...cartItem,
             quantity: cartItem.quantity + 1,
-            totalAmount: cartItem.price * (cartItem.quantity + 1)
-          }
+            totalAmount: cartItem.price * (cartItem.quantity + 1),
+          };
           newCart.push(newItem);
         } else {
           newCart.push(cartItem);
@@ -48,24 +48,22 @@ function POSPage() {
       });
 
       setCart(newCart);
-      toast(`Added ${newItem.name} to cart`, toastOptions)
-
+      toast(`Added ${newItem.name} to cart`, toastOptions);
     } else {
       let addingProduct = {
         ...product,
         'quantity': 1,
         'totalAmount': product.price,
-      }
+      };
       setCart([...cart, addingProduct]);
-      toast(`Added ${product.name} to cart`, toastOptions)
+      toast(`Added ${product.name} to cart`, toastOptions);
     }
-
-  }
+  };
 
   const removeProduct = async (product) => {
     const newCart = cart.filter(cartItem => cartItem.id !== product.id);
     setCart(newCart);
-  }
+  };
 
   const componentRef = useRef();
 
@@ -74,8 +72,8 @@ function POSPage() {
   });
 
   const handlePrint = () => {
-    handleReactToPrint();
-  }
+    setShowPrintPreview(true);
+  };
 
   useEffect(() => {
     fetchProducts();
@@ -85,11 +83,9 @@ function POSPage() {
     let newTotalAmount = 0;
     cart.forEach(icart => {
       newTotalAmount = newTotalAmount + parseInt(icart.totalAmount);
-    })
+    });
     setTotalAmount(newTotalAmount);
-  }, [cart])
-
-
+  }, [cart]);
 
   return (
     <MainLayout>
@@ -103,21 +99,15 @@ function POSPage() {
                   <img src={product.image} className="img-fluid" alt={product.name} />
                   <p>{product.price} L.L</p>
                 </div>
-
               </div>
             )}
           </div>}
-
         </div>
         <div className='col-lg-6'>
-          <div style={{ display: "none" }}>
-            <ComponentToPrint cart={cart} totalAmount={totalAmount} ref={componentRef} />
-          </div>
           <div className='table-responsive bg-dark'>
             <table className='table table-responsive table-dark table-hover'>
               <thead>
                 <tr>
-                  {/* <td>#</td> */}
                   <td>Name</td>
                   <td>Price</td>
                   <td>Qty</td>
@@ -127,7 +117,6 @@ function POSPage() {
               </thead>
               <tbody>
                 {cart ? cart.map((cartProduct, key) => <tr key={key}>
-                  {/* <td>{cartProduct.id}</td> */}
                   <td>{cartProduct.name}</td>
                   <td>{cartProduct.price}</td>
                   <td>{cartProduct.quantity}</td>
@@ -135,27 +124,26 @@ function POSPage() {
                   <td>
                     <button className='btn btn-danger btn-sm' onClick={() => removeProduct(cartProduct)}>Remove</button>
                   </td>
-
                 </tr>)
-
                   : 'No Item in Cart'}
               </tbody>
             </table>
             <h2 className='px-2 text-white'>Total Amount: L.L {totalAmount.toLocaleString()}</h2>
           </div>
-
           <div className='mt-3'>
             {totalAmount !== 0 ? <div>
               <button className='btn btn-primary' onClick={handlePrint}>
                 Place Order
               </button>
-            </div> : 'Please add a product to the cart'
-            }
+            </div> : 'Please add a product to the cart'}
+          </div>
+          <div style={{ display: showPrintPreview && cart.length > 0 ? "block" : "none" }}>
+            <ComponentToPrint cart={cart} totalAmount={totalAmount} ref={componentRef} />
           </div>
         </div>
       </div>
     </MainLayout>
-  )
+  );
 }
 
-export default POSPage
+export default POSPage;
