5582475840814f5c5ab0338e52adf8eec0bdc960
Khoder <132526568+khoderdev@users.noreply.github.com>
2024-05-09 22:32:26 +0300

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import MainLayout from '../layouts/MainLayout'
+
+function HomePage() {
+  return (
+    <MainLayout>
+        <div className='bg-light p-5 mt-4 rounded-3'>
+            <h1>Welcome to KK Coffee Shop</h1>
+            <Link to='/pos' className='btn btn-primary'>Products</Link>
+        </div>
+    </MainLayout>
+  )
+}
+
+export default HomePage
