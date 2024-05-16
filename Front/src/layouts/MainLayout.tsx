c148af6b8f24b76bd54abd2da1e69e1c0d3b286e
Khoder <132526568+khoderdev@users.noreply.github.com>
2024-05-17 00:10:17 +0300

    Performance Improvement 2

--- Front/src/layouts/MainLayout.tsx
+++ Front/src/layouts/MainLayout.tsx
@@ -1,46 +1,3 @@
-// import { Link, useNavigate } from "react-router-dom";
-
-// import { useLockContext } from "../contexts/LockContext";
-// import { FaLock } from "react-icons/fa";
-
-// function MainLayout({ children }) {
-//   const { isLocked, lockApp } = useLockContext();
-//   const navigate = useNavigate();
-
-//   const clearLock = () => {
-//     lockApp();
-//     navigate("/");
-//   };
-
-//   return (
-//     <div>
-//       <header className="bg-[#0a1627]">
-//         <nav className="container mx-auto px-4 py-2 flex justify-between items-center">
-//           <Link to="/">
-//             <img src="logo.png" className="w-72" />
-//           </Link>
-//           <div className="hidden sm:flex gap-3">
-//             <button onClick={() => navigate("/pos")}>Orders</button>
-//             <button onClick={() => navigate("/sales")}>Sales</button>
-//             <button onClick={() => navigate("/inventory")}>Inventory</button>
-//           </div>
-//           <div className="lock-icon-container" onClick={clearLock}>
-//             {!isLocked && (
-//               <FaLock
-//                 className="hover:text-[#fe0039] transition-colors"
-//                 size={32}
-//                 style={{ cursor: "pointer" }}
-//               />
-//             )}
-//           </div>
-//         </nav>
-//       </header>
-//       <main className="px-4 py-6">{children}</main>
-//     </div>
-//   );
-// }
-
-// export default MainLayout;
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useLockContext } from "../contexts/LockContext";
@@ -76,7 +33,9 @@ function MainLayout({ children }) {
     <div>
       <header
         className={`bg-[#0a1627] fixed w-full transition-transform ${
-          visible ? "transform translate-y-0" : "transform -translate-y-full"
+          visible ? "transform translate-y-0" : 
+          ""
+          // "transform -translate-y-full"
         } z-10`}
       >
         <nav className="container mx-auto px-4 py-1 flex justify-between items-center">
