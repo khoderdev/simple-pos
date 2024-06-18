a0aeef509d6c15a7af61e8976c4edcc5102a5d8e
Khoder <132526568+khoderdev@users.noreply.github.com>
2024-06-18 21:48:06 +0300

    Added CashResgister module

--- Back/productRoutes.js
+++ Back/productRoutes.js
@@ -21,6 +21,7 @@ router.get("/products", async (req, res) => {
     const products = await Product.find();
     res.json(products);
   } catch (error) {
+    console.error('Error fetching products:', error); 
     res.status(500).json({ message: error.message });
   }
 });
